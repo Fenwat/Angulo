d71956af8412f2669d2b416d20072d4bd9f2eec1
Fenwat <fenwat317@gmail.com>
2024-02-10 16:53:42 -0600

    SubInventoryGui
    -added SubInventoryGui
    -set up to take the size of the gui it sits within to create it's size

---
+++ Assets/Characters/Player/Inventory/Scenes/SubInventoryGui.gd
@@ -0,0 +1,34 @@
+extends Control
+class_name SubInventoryGui
+
+@export var inventory: PlayerInventory
+
+@onready var grid_container = $GridContainer
+@onready var slot = preload("res://Assets/GUI/Scenes/slot_gui.tscn")
+
+func _ready():
+	set_sub_inventory_parameters()
+
+func set_sub_inventory_parameters():
+	var inventory_columns = set_column_count()
+	set_y_height()
+	populate_grid(inventory_columns)
+
+func set_column_count():
+	var inventory_columns
+	var inventory_pixel_count = 84.0 # replace 84 with reference to inventory's 'size.x'
+	
+	inventory_columns = (inventory_pixel_count - 4) / 10.0
+	grid_container.columns = inventory_columns
+	return inventory_columns
+
+func set_y_height():
+	var sub_inventory_y_position = 13
+	grid_container.position.y = sub_inventory_y_position
+
+func populate_grid(columns):
+	while columns > 0:
+		var new_slot = slot.instantiate()
+		grid_container.add_child(new_slot)
+		print(columns)
+		columns -= 1
